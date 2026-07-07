b5ba0343bc072297e21682e7fa59920dee8f08bd
Tomas <castrovega80@gmail.com>
2026-07-07 18:19:40 +0200

    migrar a cloudflare

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  devIndicators: false
+  devIndicators: false,
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
